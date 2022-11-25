c802758996e9086f0bf9ec6ba6ec06caf0a98836
assbreaker <assbreak3r@gmail.com>
2022-11-25 23:32:44 +0300

    Removing emacs daemon.

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -2,10 +2,9 @@
 
 lxsession &
 nitrogen --restore &
-killall sxhkd &
-sxhkd -c $HOME/.config/i3/sxhkd/sxhkdrc &
+pgrep -x xcompmgr > /dev/null || xcompmgr &
+pgrep -x sxhkd > /dev/null || sxhkd -c $HOME/.config/i3/sxhkd/sxhkdrc &
 dunst --config $HOME/.config/dunst/dunstrc &
-/usr/bin/emacs --daemon &
+# /usr/bin/emacs --daemon &
 $HOME/.config/polybar/launch.sh &
-setxkbmap -model pc104 -layout us,ru -variant ,, -option grp:alt_shift_toggle &
-autotiling
+setxkbmap -model pc104 -layout us,ru -variant ,, -option grp:alt_shift_toggle
