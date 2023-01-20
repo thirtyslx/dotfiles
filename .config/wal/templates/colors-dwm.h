db8ad4daf14e0baf50d62cb2daee94bacb92971d
assbreaker <assbreak3r@gmail.com>
2023-01-20 20:09:17 +0300

    Add pywal config.

---
+++ .config/wal/templates/colors-dwm.h
@@ -0,0 +1,8 @@
+static const char norm_fg[] = "{color7}";
+static const char norm_bg[] = "{color0}";
+static const char norm_border[] = "{color8}";
+
+static const char sel_fg[] = "{color7}";
+static const char sel_bg[] = "{color2}";
+static const char sel_border[] = "{color7}";
+
