e0f51e10bb7364e9ed228bbe99910fd594982b75
thirtysix <assbreak3r@gmail.com>
2023-06-28 20:16:06 +0300

    Remove old stuff.

--- .config/qtile/autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-lxsession &
-nitrogen --restore &
-pgrep -x xcompmgr > /dev/null || xcompmgr &
-sxhkd || pkill -USR1 -x sxhkd &
-dunst --config $HOME/.config/dunst/dunstrc &
-setxkbmap -model pc104 -layout us,ru -variant ,, -option grp:alt_shift_toggle
